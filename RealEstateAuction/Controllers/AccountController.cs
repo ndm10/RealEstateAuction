4b167d07e65fe57458d8b80583d6ed884e08af38
Minh Nguyen <86817660+ndm10@users.noreply.github.com>
2024-09-30 20:28:07 +0700

    update

---
+++ RealEstateAuction/Controllers/AccountController.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateAuction.DAL;
+using RealEstateAuction.DataModel;
+using RealEstateAuction.Models;
+using System.Security.Claims;
+
+
+namespace RealEstateAuction.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserRepository userDAO;
+        private IMapper _mapper;
+
+        public AccountController(IMapper mapper)
+        {
+            userDAO = new UserRepository();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("profile")]
+        public IActionResult Profile()
+        {
+            //Find User by Id
+            int id = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            User user = userDAO.GetUserById(id);
+
+            //map User to UserDatalModel
+            UserDatalModel userData = _mapper.Map<User, UserDatalModel>(user);
+
+            return View(userData);
+        }
+
+        [HttpPost]
+        [Route("profile")]
+        public IActionResult Profile(UserDatalModel userData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(userData);
+            }
+            else
+            {
+                User user = _mapper.Map<UserDatalModel, User>(userData);
+                //get old user
+                User oldUser = userDAO.GetUserByEmail(user.Email);
+                //update old user
+                oldUser.FullName = user.FullName;
+                oldUser.Phone = user.Phone;
+                oldUser.Dob = user.Dob;
+                oldUser.Address = user.Address;
+
+                userDAO.UpdateUser(oldUser);
+                TempData["Message"] = "Cập nhật thông tin cá nhân thành công!";
+                return View(userData);
+            }
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("change-password")]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("change-password")]
+        public IActionResult ChangePassword(ChangePasswordModel passwordData)
+        {
+            //if value of user enter is not valid
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                //find User by Id
+                int id = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = userDAO.GetUserById(id);
+
+                //verify old password of user
+                if (!user.Password.Equals(passwordData.Password))
+                {
+                    ModelState.AddModelError("Password", "Mật khẩu cũ không đúng!");
+                    return View();
+                }
+                else
+                {
+                    //update new password
+                    userDAO.UpdatePassword(user.Email, passwordData.NewPassword);
+                    TempData["Message"] = "Thay đổi mật khẩu thành công!";
+                    return View();
+                }
+            }
+        }
+    }
+}
